aa93e5c097d90857f051fa3c5b205593a7c15415
changkaizhao <changkaizhao1006@gmail.com>
2018-11-11 00:30:09 +0800

    edit app.js and index.html

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-console.log('Hello world');
-var a = 1;
-a = a + 1;
-console.log(a);
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
