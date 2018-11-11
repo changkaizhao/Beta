2d91fa1939d808afb99cecad24b07096394fabb6
changkaizhao <changkaizhao1006@gmail.com>
2018-11-11 19:11:27 +0800

    socket io

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ app.get('/', function(req, res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
+
+console.log("Server started.");
+
+var io = require('socket.io')(serv, {});
+
+io.sockets.on('connection', function(socket){
+    console.log('socket connection');
+});
